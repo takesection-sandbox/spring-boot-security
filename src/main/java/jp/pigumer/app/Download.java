c0e0ed4e70ca295b95eb42410b7906bb211eaa79
S Shoji <shige@pigumer.gr.jp>
2016-07-05 22:47:20 +0900

    add download

---
+++ src/main/java/jp/pigumer/app/Download.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2016 Pigumer Group Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jp.pigumer.app;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/download")
+public class Download {
+
+    @RequestMapping
+    public ResponseEntity<byte[]> download() throws Exception {
+        byte[] bytes = "こんにちは".getBytes("MS932");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-Type", "text/plain; charset=MS932");
+
+        return new ResponseEntity<byte[]>(bytes, headers, HttpStatus.OK);
+    }
+
+}
