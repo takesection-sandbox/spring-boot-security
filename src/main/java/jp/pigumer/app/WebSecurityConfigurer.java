d77721b80fdca9384b40d68a7285ea707c4ad124
Shigeki Shoji <shige@pigumer.gr.jp>
2016-08-03 00:15:43 +0900

    Update Error Pages

--- src/main/java/jp/pigumer/app/WebSecurityConfigurer.java
+++ src/main/java/jp/pigumer/app/WebSecurityConfigurer.java
@@ -36,7 +36,7 @@ public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/404", "/css/**").permitAll()
+                .antMatchers("/css/**").permitAll()
                 .anyRequest().fullyAuthenticated()
 
                 .and().formLogin()
